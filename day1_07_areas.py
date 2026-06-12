6c3e2507cc0c983967766dd87182a68dd52969eb
akshay-python-dev <akshayavarshinik7226@gmail.com>
2026-06-12 09:05:30 +0530

    Create day1_07_areas.py

---
+++ day1_07_areas.py
@@ -0,0 +1,9 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+area_of_traingle=(a*b)/2
+area_of_square=a*a
+area_of_rectangle=a*c
+print(area_of_traingle)
+print(area_of_square)
+print(area_of_rectangle)
