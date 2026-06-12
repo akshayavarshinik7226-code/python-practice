2b4761960ee4fbfb71d4af5220fdadf295c3f35f
akshay-python-dev <akshayavarshinik7226@gmail.com>
2026-06-12 21:23:22 +0530

    Delete day1_07_areas.py

--- day1_07_areas.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a"))
-b=int(input("enter b"))
-c=int(input("enter c"))
-area_of_traingle=(a*b)/2
-area_of_square=a*a
-area_of_rectangle=a*c
-print(area_of_traingle)
-print(area_of_square)
-print(area_of_rectangle)
